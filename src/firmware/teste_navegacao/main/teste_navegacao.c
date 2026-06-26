be12dd0500cd8ddf234e8261ba33b2c602d2f837
EuAmoMilho <noboru.luiz.wm@gmail.com>
2026-06-26 19:54:33 -0300

    descomentando alteracoes

--- src/firmware/teste_navegacao/main/teste_navegacao.c
+++ src/firmware/teste_navegacao/main/teste_navegacao.c
@@ -14,7 +14,7 @@
 #include "power_module.h"
 #include "infrared.h"
 
-// #include "telemetry_data_nvs.h"
+#include "telemetry_data_nvs.h"
 
 static const char *TAG = "teste_navegacao";
 
@@ -42,9 +42,9 @@ pose_t pose;
 encoder_t encoderR;
 encoder_t encoderL;
 
-// ina226_t ina;
+ina226_t ina;
 
-// static i2c_master_bus_handle_t bus_handle;
+static i2c_master_bus_handle_t bus_handle;
 
 motor_t motorR = { .pwm_gpio1 = PWM_R1, .pwm_gpio2 = PWM_R2};
 motor_t motorL = { .pwm_gpio1 = PWM_L1, .pwm_gpio2 = PWM_L2};
@@ -72,30 +72,30 @@ static void IRAM_ATTR button_isr_handler(void *arg)
     }
 }
 
-// void telemetry_task(void *arg)
-// {
-//     float voltage_v = 0.0;
-//     float current_a = 0.0;
+void telemetry_task(void *arg)
+{
+    float voltage_v = 0.0;
+    float current_a = 0.0;
 
-//     while (1)
-//     {
-//         // 1. Lemos os dados reais do sensor passando o endereço das variáveis (&)
-//         ina226_get_bus_voltage(&ina, &voltage_v);
-//         ina226_get_current(&ina, &current_a);
+    while (1)
+    {
+        // 1. Lemos os dados reais do sensor passando o endereço das variáveis (&)
+        ina226_get_bus_voltage(&ina, &voltage_v);
+        ina226_get_current(&ina, &current_a);
 
-//         // 2. Convertemos os floats (Volts/Amperes) para inteiros (Milivolts/Miliamperes)
-//         // O "cast" (uint16_t) garante que o ESP32 descarte as casas decimais corretamente
-//         uint16_t voltage_mv = (uint16_t)(voltage_v * 1000.0f);
-//         int16_t current_ma = (int16_t)(current_a * 1000.0f);
+        // 2. Convertemos os floats (Volts/Amperes) para inteiros (Milivolts/Miliamperes)
+        // O "cast" (uint16_t) garante que o ESP32 descarte as casas decimais corretamente
+        uint16_t voltage_mv = (uint16_t)(voltage_v * 1000.0f);
+        int16_t current_ma = (int16_t)(current_a * 1000.0f);
 
-//         // 3. Salvamos na memória NVS usando a função do Canvas
-//         telemetry_save_sample(voltage_mv, current_ma);
+        // 3. Salvamos na memória NVS usando a função do Canvas
+        telemetry_save_sample(voltage_mv, current_ma);
 
-//         // 4. Aguardamos 500ms antes de ler novamente (2 amostras por segundo)
-//         // Isso evita encher a memória muito rápido
-//         vTaskDelay(pdMS_TO_TICKS(500));
-//     }
-// }
+        // 4. Aguardamos 500ms antes de ler novamente (2 amostras por segundo)
+        // Isso evita encher a memória muito rápido
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
+}
 
 void buzzer_init(){
 
@@ -188,14 +188,14 @@ void app_main(void)
 
     buzzer_init();
 
-    // i2c_master_bus_config_t bus_config = {
-    //     .i2c_port = I2C_PORT,
-    //     .sda_io_num = I2C_SDA,
-    //     .scl_io_num = I2C_SCL,
-    //     .clk_source = I2C_CLK_SRC_DEFAULT
-    // };
+    i2c_master_bus_config_t bus_config = {
+        .i2c_port = I2C_PORT,
+        .sda_io_num = I2C_SDA,
+        .scl_io_num = I2C_SCL,
+        .clk_source = I2C_CLK_SRC_DEFAULT
+    };
     
-    // ESP_ERROR_CHECK(i2c_new_master_bus(&bus_config, &bus_handle));
+    ESP_ERROR_CHECK(i2c_new_master_bus(&bus_config, &bus_handle));
     
     play_tone(A4_FREQ, TEMPO);
 
@@ -208,33 +208,33 @@ void app_main(void)
 
     play_tone(A3_FREQ, 50);
 
-    // esp_err_t err = ina226_init(
-    //     &ina, 
-    //     bus_handle,
-    //     INA_ADDRESS, 
-    //     SHUNT, 
-    //     MAX_CURRENT
-    // );
+    esp_err_t err = ina226_init(
+        &ina, 
+        bus_handle,
+        INA_ADDRESS, 
+        SHUNT, 
+        MAX_CURRENT
+    );
     
-    // if (err == ESP_OK) {
-    //     ESP_LOGI(TAG, "INA226 inicializado com sucesso!");
-    // } else {
-    //     ESP_LOGE(TAG, "Falha ao inicializar INA226");
-    // }   
+    if (err == ESP_OK) {
+        ESP_LOGI(TAG, "INA226 inicializado com sucesso!");
+    } else {
+        ESP_LOGE(TAG, "Falha ao inicializar INA226");
+    }   
 
-    // play_tone(E4_FREQ, TEMPO);
+    play_tone(E4_FREQ, TEMPO);
 
     vTaskDelay(pdMS_TO_TICKS(1000));
 
     play_tone(E4_FREQ, 50);
 
-    // vTaskDelay(pdMS_TO_TICKS(100));
+    vTaskDelay(pdMS_TO_TICKS(100));
 
-    // play_tone(E4_FREQ, 50);
+    play_tone(E4_FREQ, 50);
 
-    // ESP_ERROR_CHECK(telemetry_init());
+    ESP_ERROR_CHECK(telemetry_init());
 
-    // play_tone(A4_FREQ, TEMPO);
+    play_tone(A4_FREQ, TEMPO);
 
     driver_init();
 
@@ -273,19 +273,19 @@ void app_main(void)
     &mission_task_handle
     );
 
-    // // criacao da task que vai ficar lendo o INA226 e salvando em segundo plano
-    // xTaskCreate(
-    //     telemetry_task,
-    //     "telemetry_task",
-    //     4096,
-    //     NULL,
-    //     2, // Prioridade baixa, para não atrapalhar os motores e sensores
-    //     NULL
-    // );
+    // criacao da task que vai ficar lendo o INA226 e salvando em segundo plano
+    xTaskCreate(
+        telemetry_task,
+        "telemetry_task",
+        4096,
+        NULL,
+        2, // Prioridade baixa, para não atrapalhar os motores e sensores
+        NULL
+    );
 
-    // telemetry_print_all();
+    telemetry_print_all();
 
-    // ESP_ERROR_CHECK(telemetry_clear());
+    ESP_ERROR_CHECK(telemetry_clear());
 
     while(1){
         vTaskDelay(pdMS_TO_TICKS(3000));
