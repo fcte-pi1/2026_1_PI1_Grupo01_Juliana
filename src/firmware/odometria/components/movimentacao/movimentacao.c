68fe4d30bb32c8b0e45f629c56f532647e8402b4
EuAmoMilho <noboru.luiz.wm@gmail.com>
2026-06-26 20:44:08 -0300

    mudancas movimentacao (1/2)

--- src/firmware/odometria/components/movimentacao/movimentacao.c
+++ src/firmware/odometria/components/movimentacao/movimentacao.c
@@ -60,16 +60,29 @@ void mouse_break(motor_t *motorR, motor_t *motorL){
     ESP_LOGI(TAG, "comando frear");
 }
 
+float mouse_get_linear_speed(encoder_t *encR, encoder_t *encL, int64_t dt){
+    float   spdR = encoder_get_v(encR, dt, RAIO_R),
+            spdL = encoder_get_v(encL, dt, RAIO_R);
+        
+    float lin_spd = (spdR + spdL)/2;
+
+    return lin_spd;
+}
+
 //funcoes de movimentacao
 
 void movimentacao_move_cell(motor_t *mtrR, motor_t *mtrL, encoder_t *encR, encoder_t *encL, pose_t *pos){
+    float time, previoustime, meanspeed;
+
+    previoustime = odometria_get_segundos();
+
     float target = L_CELULA_CM;
 
     float desloc = 0,
           desloc_R = 0,
           desloc_L = 0;
 
-    // int speed = BASE_SPD;
+    // int motor_speed = BASE_SPD;
 
     mouse_movefwd(mtrR, mtrL);
 
@@ -77,13 +90,12 @@ void movimentacao_move_cell(motor_t *mtrR, motor_t *mtrL, encoder_t *encR, encod
 
         /* //possivel mudança: controle bem rudimentar (com risco de explodir a velocidade)
         if (desloc_R-desloc_L >= 5) {
-            speed = speed+5;
-            motor_set_speed(mtrL, speed);
+            motor_speed = motor_speed+5;
+            motor_set_speed(mtrL, motor_speed);
         } else if (desloc_R-desloc_L <= 5) {
-            speed = speed+5;
-            motor_set_speed(mtrR, speed) 
+            motor_speed = motor_speed+5;
+            motor_set_speed(mtrR, motor_speed) 
         }
-        
         */
 
         desloc_R += encoder_get_deslocamento(encR, RAIO_R);
@@ -96,8 +108,17 @@ void movimentacao_move_cell(motor_t *mtrR, motor_t *mtrL, encoder_t *encR, encod
 
     mouse_break(mtrR, mtrL);
     
+    time = odometria_get_segundos() - previoustime;
+
+    meanspeed = mouse_get_linear_speed(encR, encL, time);
+
+    pos->cell_count++;
+
+    odometria_update_vm(pos, meanspeed);
+
     odometria_update_xy(pos, desloc);
-    ESP_LOGI(TAG, "andou 1 celula, desloc: %f", desloc);
+    
+    ESP_LOGI(TAG, "andou 1 celula, posicao: (%f, %f), desloc: %f, velocidade media (m/s):%f",pos->x, pos->y, desloc, meanspeed);
 }
 
 void movimentacao_turn_clws(motor_t *mtrR, motor_t *mtrL, encoder_t *encR, encoder_t *encL, pose_t *pos){
