61835a4b2ed9dc14bee24b98c9337a7d381fd6bc
EuAmoMilho <noboru.luiz.wm@gmail.com>
2026-06-29 12:16:26 -0300

    correcoes de bugs

--- src/firmware/odometria/components/odometria/odometria.c
+++ src/firmware/odometria/components/odometria/odometria.c
@@ -36,7 +36,7 @@ void odometria_update_xy(pose_t *pose, float deslocamento){
 }
 
 void odometria_update_vm(pose_t *pose, float vm_novo){
-    float sum_vm = pose->vm + vm_novo;
+    float sum_vm = (pose->vm * (pose->cell_count -1)) + vm_novo;
 
     pose->vm = sum_vm / pose->cell_count;
 }
@@ -101,7 +101,7 @@ const char* odometria_orientacao_string(orientacao_t o){
 float odometria_get_segundos(){
     int64_t tempo_atual = esp_timer_get_time();
 
-    float time = tempo_atual/1000000;
+    float time = tempo_atual/1000000.0f;
 
     return time;
 }
@@ -115,8 +115,4 @@ void odometria_pos_init(pose_t *pose, float x0, float y0, orientacao_t orientaca
     pose->cell_count = 0;
 
     ESP_LOGI(TAG, "INICIO: (%f, %f) ORIENTACAO %s", pose->x, pose->y, odometria_orientacao_string(pose->orientacao));
-}
-
-void odometria_move_cell(){
-
 }
